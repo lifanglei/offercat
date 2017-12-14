6d1a1406374ff82ca8c7573590529a4796d7b6a5
Rendong Liu <rendong_liu@hotmail.com>
2017-12-15 01:42:37 +0800

    change image size2

--- my-app/src/components/banner.js
+++ my-app/src/components/banner.js
@@ -16,9 +16,9 @@ class CompanyCard extends React.Component {
     const {company,history}= this.props;
     return (
       <div className="ms-thumbnail-container">
-        <figure className="ms-thumbnail-horizontal">
+        <figure className="ms-thumbnail ms-thumbnail-horizontal">
           <div className="img-wrapper">
-          <img className="logo" style={{  height:'120px'}} src={company.photo_url} alt="暂无公司logo"/>
+          <img className="logo" style={{  height:'120px',    width: 'initial'}} src={company.photo_url} alt="暂无公司logo"/>
           </div>
             <div className="caption">{company.name}</div>
           <figcaption className="ms-thumbnail-caption text-center">
